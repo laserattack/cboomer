d9ad00e651e3a386bfa0e9a552badabb69dfa32d
serr <sc7227484@gmail.com>
2026-03-15 21:18:08 +0300

    comments in config.h

--- config.h
+++ config.h
@@ -2,16 +2,17 @@
 #define CONFIG_H
 
 typedef struct {
-    float minScale;
-    float scrollSpeed;
-    float dragFriction;
-    float scaleFriction;
-    float initialFlDeltaRadius;
-    float velocityThreshold;
+    float minScale;             // Minimum allowed zoom scale (prevents zooming out too far)
+    float scrollSpeed;          // Speed of zoom when scrolling or using +/- keys
+    float dragFriction;         // Friction coefficient for camera movement inertia (higher = faster stop)
+    float scaleFriction;        // Friction coefficient for zoom inertia (higher = faster stop)
+    float initialFlDeltaRadius; // Initial delta value for flashlight radius change per Ctrl+scroll
+    float velocityThreshold;    // Minimum velocity to apply inertia (prevents micro-movements)
 } Config;
 
 #ifdef CONFIG_IMPL
 
+// YOU CAN HACK THIS VALUES
 Config defaultConfig = {
     .minScale             = 0.5f,
     .scrollSpeed          = 1.5f,
